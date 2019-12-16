8497d19b42113a01d880f1c8d8e37e3440b03f38
zxh0 <zhangxiuhong2013@gmail.com>
2019-12-16 09:38:43 +0800

    merge evm

---
+++ classpy-evm/src/main/java/com/github/zxh/classpy/ethereum/EvmBinComponent.java
@@ -0,0 +1,21 @@
+package com.github.zxh.classpy.ethereum;
+
+import com.github.zxh.classpy.common.FileComponent;
+
+public class EvmBinComponent extends FileComponent {
+
+    public final void read(EvmBinReader reader) {
+        int offset = reader.getPosition();
+        readContent(reader);
+        int length = reader.getPosition() - offset;
+        super.setOffset(offset);
+        super.setLength(length);
+    }
+
+    protected void readContent(EvmBinReader reader) {
+        for (FileComponent bc : getComponents()) {
+            ((EvmBinComponent) bc).read(reader);
+        }
+    }
+
+}
